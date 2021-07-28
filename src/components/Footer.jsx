6a7cf4fd14ff8535aac44aff1860fe8c5db3101b
shubham-bhuvad <sbhuvad444@gmail.com>
2021-07-29 01:11:32 +0530

    make it better

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer(props) {
+  return (
+    <div className="footer">
+      <p>Ⓒcopyright 2021 by Shubham-Bhuvad</p>
+    </div>
+  );
+}
+
+export default Footer;
